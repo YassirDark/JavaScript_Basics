7d5b2783e54b153ee9ef65fcb960178a146d4f3f
Yassir CHENNAOUI <122177015+YassirDark@users.noreply.github.com>
2024-01-31 15:13:04 +0100

    Operators_and_Variables.js

--- Operators.js
+++ Operators.js
@@ -16,9 +16,29 @@ console.log('The value of levelUp:', levelUp);
 console.log('The value of powerLevel:', powerLevel); 
 console.log('The value of multiplyMe:', multiplyMe); 
 console.log('The value of quarterMe:', quarterMe);
+
 // increment operator (++) and decrement operator (--).
 let gainedDollar = 3;
 let lostDollar = 50;
 
 gainedDollar++;
 lostDollar--;
+
+//String Concatenation with Variables
+let favoriteAnimal ='Lion'
+console.log('My favorite animal is: ' + favoriteAnimal)
+
+/*String Interpolation*/
+let myName ='Yassir';
+let myCity ='NewYork';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`); //`` is [alt 096] and not ''   to use the ${} methode
+
+//typeof 
+const unknown1 = 'LoL';
+console.log(typeof unknown1); // Output: string
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+const unknown3 = true; 
+console.log(typeof unknown3); // Output: boolean
+
+
